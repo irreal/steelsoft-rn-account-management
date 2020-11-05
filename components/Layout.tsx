a2cba1ec566ca3c7ea3e789d1ba8079db7e19c93
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-11-05 18:02:52 +0100

    Improve layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,36 +1,51 @@
 import React, { ReactNode } from 'react'
-import Link from 'next/link'
 import Head from 'next/head'
-
+import { Avatar, Box, Button, Flex, Text } from '@chakra-ui/core'
+import useUser from '../data/useUser'
+import { logout } from '../services/auth'
+import Router from "next/router";
 type Props = {
   children?: ReactNode
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Upravljanje korisnicima</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>O Aplikaciji</a>
-        </Link>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      {/* <span>I'm here to stay (Footer)</span> */}
-    </footer>
-  </div>
-)
+const Layout = ({ children, title = 'This is the default title' }: Props) => {
+  const user = useUser();
+  return (
+    <Box h="100vh" d="flex" flexDir="column">
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Flex direction={["column", "row"]} flexGrow={1} >
+        <header>
+          <Flex as="nav" h={["auto", "100vh"]} direction={["row", "column"]} px={[4, 1]} py={[1, 4]}>
+            <Box my={4} d="flex" flexDir="column" alignItems="center">
+              <Avatar name={user.user?.email}></Avatar>
+              <Text fontSize="xs">{user.user?.email}</Text>
+            </Box>
+            <Box d="flex" flexDir={["row-reverse", "column-reverse"]} alignItems="center" flexGrow={1}>
+              <Button onClick={() => {
+                logout().then(() => {
+                  Router.replace("/");
+                });
+              }}>
+                Izloguj se
+              </Button>
+            </Box>
+          </Flex>
+        </header>
+        <Box bg="#EBE7F7" flexGrow={1} p={4}>
+          {children}
+        </Box>
+      </Flex>
+      <footer>
+        <hr />
+        <Text px={8}>SteelSoft RN Administracija korisnika</Text>
+      </footer>
+    </Box>
+  );
+}
 
 export default Layout
