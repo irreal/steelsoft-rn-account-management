801e799710b3f1233514a8acea3a07feb8cd8e7a
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-11-05 18:30:19 +0100

    implement initial table for user list

--- components/UserList.tsx
+++ components/UserList.tsx
@@ -1,5 +1,32 @@
+import DataTable from "react-data-table-component";
 import useUsers from "../data/useUsers";
+import { User } from "../interfaces";
 import UserRow from './UserRow';
+
+const columns = [
+    {
+        name: 'Email',
+        selector: 'email',
+        sortable: true,
+    },
+    {
+        name: 'Datum kreiranja',
+        selector: 'creationTime',
+        sortable: true,
+    },
+    {
+        name: 'Deaktiviran',
+        selector: 'disabled',
+        sortable: true,
+        cell: (row: any) => (row.disabled ? "da" : "ne")
+    },
+    {
+        name: 'Admin',
+        selector: 'admin',
+        sortable: true,
+        cell: (row: any) => (row.admin ? "da" : "ne")
+    },
+];
 
 const UserList = () => {
     const { users, error, loading } = useUsers();
@@ -10,14 +37,27 @@ const UserList = () => {
         return <div> puklo u učitavanju :( {JSON.stringify(error)}</div>
     }
 
-    const userComponents = [];
-    if (users && users.length > 0) {
-        userComponents.push(...users.map(u => <UserRow key={u.uid} user={u} />));
-    }
-    else {
-        userComponents.push(<div>nema usera ?</div>);
-    }
-    return <>{userComponents}</>
+    return (
+        <div>
+            <DataTable
+                title="Korisnici"
+                columns={columns}
+                data={users as User[]}
+                defaultSortField="Email"
+                expandableRows
+                expandOnRowClicked
+                highlightOnHover
+                striped
+            />
+        </div>);
+    // const userComponents = [];
+    // if (users && users.length > 0) {
+    //     userComponents.push(...users.map(u => <UserRow key={u.uid} user={u} />));
+    // }
+    // else {
+    //     userComponents.push(<div>nema usera ?</div>);
+    // }
+    // return <>{userComponents}</>
 }
 
 export default UserList
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,20 +1,12 @@
 import Layout from '../components/Layout'
-import { useRecoilState } from 'recoil'
-import { textState } from '../atoms/textState'
-import { Button } from '@chakra-ui/core';
-import { logout } from '../services/auth';
 import useUser from '../data/useUser';
 import { useEffect } from 'react';
 import Router from "next/router";
 import UserList from '../components/UserList';
 
 const DashboardPage = () => {
-  const [text, setText] = useRecoilState(textState);
-  const onChange = (event: any) => {
-    setText(event.target.value);
-  };
 
-  const { user, loading, loggedOut, mutate } = useUser();
+  const { loading, loggedOut } = useUser();
 
   useEffect(() => {
     if (loggedOut) {
@@ -25,14 +17,6 @@ const DashboardPage = () => {
   if (loading) return "loading...";
   return (
     <Layout title="SteelSoft RN - Admin">
-      <h1>Hello Next.js 👋 qwe {text}</h1>
-      <input type="text" value={text} onChange={onChange} />
-      <p>email je: {user?.email}</p>
-      <Button onClick={() => {
-        logout().then(() => {
-          mutate();
-        });
-      }}>Izloguj se</Button>
       <UserList />
     </Layout>
   )
